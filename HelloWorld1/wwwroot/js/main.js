e9eddf669e68cceeab62b674fbd06de38690e53b
onlinekkabchi <onlinekkabchi@gmail.com>
2023-08-12 22:20:21 +0900

    tank 모델 랜더링 성공

--- HelloWorld1/wwwroot/js/main.js
+++ HelloWorld1/wwwroot/js/main.js
@@ -8,53 +8,46 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 //const controls = new OrbitControls(camera, renderer.domElement);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa0a0a0);
+//scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
+
 const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / 2) / window.innerHeight, 0.1, 1000);
-//camera.position.set(0, 0, 100);
-//camera.lookAt(0, 0, 0);
+//camera.position.set(0, 0, 0);
+camera.lookAt(0, 0, 0);
+camera.position.z = 20;
 const renderer = new THREE.WebGLRenderer();
 
 const loader = new GLTFLoader();
 loader.load('/static/model/Tiger_I.glb', function (gltf) {
-    scene.add(gltf.scene);
+    const model = gltf.scene;
+    model.position.set(0, 0, 0);
+    model.scale.set(1, 1, 1);
+    scene.add(model);
+
+    const animations = gltf.animations;
+    let mixer = new THREE.AnimationMixer(model);
+    mixer.clipAction(animations[0]).play();
+
 }, undefined, function (err) {
     console.error(err)
 })
 
-//console.log('rendering');
-
 renderer.setSize(window.innerWidth / 2, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
 // 큐브 그리기
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
+//const geometry = new THREE.BoxGeometry(1, 1, 1);
+//const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//const cube = new THREE.Mesh(geometry, material);
+//scene.add(cube);
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
 
 animate();
-
-// 라인 그리기
-//const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-//const points = [];
-//points.push(new THREE.Vector3(-10, 0, 0));
-//points.push(new THREE.Vector3(-0, 10, 0));
-//points.push(new THREE.Vector3(10, 0, 0));
-
-//const geometry = new THREE.BufferGeometry().setFromPoints(points);
-//const line = new THREE.Line(geometry, material);
-
-//scene.add(line);
-//renderer.render(scene, camera);
-// You should now be seeing an arrow pointing upwards, made from two blue lines.
